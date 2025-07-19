5e0e269c4b6cbaa3395b5a406b940b501c807362
Rithika Pillai <83540307+CrazyScientist1901@users.noreply.github.com>
2025-07-19 23:02:45 +0530

    Create firebase.js

---
+++ public/firebase.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyD4mTQJ3pEjkaNrOV5DF7Ixmm-nrnIrkxw",
+    authDomain: "mathgame-d8afe.firebaseapp.com",
+    databaseURL: "https://mathgame-d8afe-default-rtdb.firebaseio.com",
+    projectId: "mathgame-d8afe",
+    storageBucket: "mathgame-d8afe.appspot.com",
+    messagingSenderId: "339478386785",
+    appId: "1:339478386785:web:fe25feb5bcb940c8983220",
+    measurementId: "G-DPJ79JZFQ1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
